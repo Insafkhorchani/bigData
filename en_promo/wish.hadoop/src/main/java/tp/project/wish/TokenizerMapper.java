04e678d99b4ade9f3a818118009c3fdffd00b9c2
InsafKhorchani <insafkh1997@gmail.com>
2021-01-17 21:21:23 +0100

    map_reduce en promition

---
+++ en_promo/wish.hadoop/src/main/java/tp/project/wish/TokenizerMapper.java
@@ -0,0 +1,30 @@
+package tp.project.wish;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+
+    public class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
+
+        private  static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(Object key, Text value, Mapper.Context context
+        ) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString(),",");
+
+               String[] l = value.toString().split(",");
+               Integer promo = Integer.valueOf(l[3].toString());
+
+                 word.set( l[0].toString() );
+
+            one = new IntWritable(promo);
+                   context.write(word,one );}
+
+    }
+
+
+
